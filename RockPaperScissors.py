c06d67cee52ccb2b03a3223147221971fc6b2749
Christian Tanicala <christian.tanicala@gmail.com>
2023-02-13 22:54:51 -0500

    Added scissors, and paper functionality with replay and exit functionality

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -7,9 +7,11 @@ import random
 
 while True:
     choices = ["rock", "paper", "scissors"]
+    replay_choices = ["yes", "no"]
     player_selection = None
     computer_selection = random.choice(choices)
     total_score = 0
+    replay_game = None
 
     while player_selection not in choices: 
         player_selection = input("Please select rock, paper, or scissors: ").lower()
@@ -18,7 +20,7 @@ while True:
         print("Player: " + player_selection)
         print("Computer: " + computer_selection)
         print("Tie!")
-    if player_selection == "rock":
+    elif player_selection == "rock":
             if computer_selection == "paper": 
                 print("Player: " + player_selection)
                 print("Computer: " + computer_selection)
@@ -30,7 +32,42 @@ while True:
                 print("Player: " + player_selection)
                 print("Computer: " + computer_selection)
                 print("You Won!")
-                
+
+                total_score = total_score +1
+                print("Your total score is " + str(total_score))
+    elif player_selection == "scissors":
+            if computer_selection == "rock": 
+                print("Player: " + player_selection)
+                print("Computer: " + computer_selection)
+                print("You Lose!")
+
+                total_score = total_score -1
+                print("Your total score is " + str(total_score))
+            if computer_selection == "paper":
+                print("Player: " + player_selection)
+                print("Computer: " + computer_selection)
+                print("You Won!")
+
+                total_score = total_score +1
+                print("Your total score is " + str(total_score))
+    elif player_selection == "paper":
+            if computer_selection == "scissors": 
+                print("Player: " + player_selection)
+                print("Computer: " + computer_selection)
+                print("You Lose!")
+
+                total_score = total_score -1
+                print("Your total score is " + str(total_score))
+            if computer_selection == "rock":
+                print("Player: " + player_selection)
+                print("Computer: " + computer_selection)
+                print("You Won!")
+
                 total_score = total_score +1
                 print("Your total score is " + str(total_score))
 
+    while replay_game not in replay_choices:
+        replay_game =  input("Play Again? Yes/No: ").lower()
+    
+    if replay_game == "no":
+         break
